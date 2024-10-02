73e211c0fd2a9ef80ec360aa8d7fc822e7348ae0
dhyanpatel3 <139840417+dhyanpatel3@users.noreply.github.com>
2024-10-02 14:25:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import TicTacToe from "./components/tic-tac-toe";
+
+
+function App() {
+
+
+  return  (
+    < TicTacToe/>
+  )
+   
+    
+  
+}
+
+export default App;
---
+++ src/components/tic-tac-toe.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import useTictacToe from "../hooks/use-tic-tac-toe";
+
+function TicTacToe() {
+ 
+const{board ,handleClick, calculateWinner, resetGame , getStatusMessage}= useTictacToe() 
+
+  return  (
+    <div className="heading">
+        <h1 >Tic-Tac-Toe</h1>
+    <div className="game">
+      
+      <div className="status">
+       {getStatusMessage()}
+        <button className="reset-button" onClick={resetGame}>Reset Game</button>
+      </div>
+
+      <div className="board">
+        {board.map((b,index) =>{
+          return <button className="cell " key={index} onClick={()=> handleClick(index) } disabled= {b != null}>{b}</button>
+        })}
+      </div>
+    </div>
+    </div>
+  )
+   
+    
+  
+}
+
+export default TicTacToe;
